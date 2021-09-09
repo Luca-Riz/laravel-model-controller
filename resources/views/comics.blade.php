e4c2fdd033f439e922bce8b65e27fd58101b7039
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-09 09:13:28 +0200

    add graphic object on comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,6 +1,16 @@
 @foreach( $fumetti as $comic )
 
     <p>{{$comic->title}}</p>
+    <p>{{$comic->description}}</p>
+    <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+    <p>{{$comic->price}}</p>
+    <p>{{$comic->series}}</p>
+    <p>{{$comic->sale_data}}</p>
+    <p>{{$comic->type}}</p>
+
+
+
+    {{-- @dd($comic) --}}
 
 
 
